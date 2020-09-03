003dade51463f64802a6cbc1523a683d13b79e77
Connor Blakeney <connorblakeney@yahoo.com>
2020-09-03 13:20:46 -0500

    added products directory

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect } from "react"
 import { LocationContext } from "./LocationProvider"
-import { Location } from "./Location"
 import "./Location.css"
 
 export const LocationList = () => {
